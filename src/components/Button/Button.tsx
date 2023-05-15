910dc7c8ab35d2058b1321fda086d1164f9765be
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-16 03:52:50 +0400

    Create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import styles from './Button.module.css';
+
+export type ButtonPropsType = {
+    name: string;
+    disabled?: boolean;
+    onClick: () => void;
+};
+
+export const Button: FC<ButtonPropsType> = ({
+                                                name,
+                                                disabled = false,
+                                                onClick,
+                                            }) => {
+    return (
+        <button className={styles.button} disabled={disabled} onClick={onClick}>
+            {name}
+        </button>
+    );
+};
