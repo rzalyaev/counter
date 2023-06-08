73ae0e28480a9dad0d54ca355cc657d0f6672608
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-08 21:03:30 +0400

    Add buttons disabling when settings are open

--- src/components/Counter/Controls/Controls.tsx
+++ src/components/Counter/Controls/Controls.tsx
@@ -6,25 +6,27 @@ type PropsType = {
     count: number
     minValue: number
     maxValue: number
+    showSettings: boolean
     increase: () => void
     decrease: () => void
     reset: () => void
     changeSettingsState: () => void
 }
 
-export const Controls = ({count, minValue, maxValue, increase, decrease, reset, changeSettingsState}: PropsType) => {
+export const Controls = ({count, minValue, maxValue, showSettings, increase,
+                             decrease, reset, changeSettingsState}: PropsType) => {
     return (
         <div className={styles.buttons}>
             <Button name={'INC'}
-                    disabled={count === maxValue}
+                    disabled={count === maxValue || showSettings}
                     onClick={increase}
             />
             <Button name={'DEC'}
-                    disabled={count === 0 || count === minValue}
+                    disabled={count === 0 || count === minValue || showSettings}
                     onClick={decrease}
             />
             <Button name={'RESET'}
-                    disabled={count === 0 || count === minValue}
+                    disabled={count === 0 || count === minValue || showSettings}
                     onClick={reset}
             />
             <Button name={'SETTINGS'}
