74bc95f6a0aa1e0ed85def9032c54fe0671316c8
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-16 22:43:16 +0400

    Add disabled state to props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,10 +5,11 @@ type ButtonPropsType = {
   title: string
   onClickHandler: () => void
   className?: string
+  disabled?: boolean
 }
 
-export const Button = ({title, onClickHandler, className}: ButtonPropsType) => {
+export const Button = ({title, onClickHandler, className, disabled = false}: ButtonPropsType) => {
   return (
-      <button onClick={onClickHandler} className={className}>{title}</button>
+      <button onClick={onClickHandler} className={className} disabled={disabled}>{title}</button>
   );
 };
